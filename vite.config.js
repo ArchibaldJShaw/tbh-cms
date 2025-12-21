58d19f87169654f6b79b326e155bfdf344399d8c
ArchibaldShaw <archybald.shou@finsight.com>
2025-12-22 00:20:38 +0200

    Disable Vite host checking entirely

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 1337,
-    allowedHosts: [
-      'tbh-cms-production.up.railway.app',
-      'localhost',
-      '127.0.0.1',
-      '0.0.0.0'
-    ]
+    allowedHosts: 'all'
   }
 });
